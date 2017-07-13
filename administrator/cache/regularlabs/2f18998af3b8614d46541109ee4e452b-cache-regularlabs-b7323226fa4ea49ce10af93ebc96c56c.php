1fd902ff92777d9bd87d77e8c2bb689d6cd076c3
Ray Selby <ray@goaheadspace.com>
2017-07-13 20:47:34 +0200

    Add files via upload

---
+++ administrator/cache/regularlabs/2f18998af3b8614d46541109ee4e452b-cache-regularlabs-b7323226fa4ea49ce10af93ebc96c56c.php
@@ -0,0 +1,17 @@
+<?php die("Access Denied"); ?>#x#s:1339:"<?xml version="1.0" encoding="utf-8"?>
+<extensions>
+	<extension>
+		<name>Advanced Module Manager</name>
+		<id>advancedmodulemanager</id>
+		<alias>advancedmodulemanager</alias>
+		<extname>advancedmodules</extname>
+		<version>7.1.4</version>
+		<has_pro>1</has_pro>
+		<pro>0</pro>
+		<downloadurl>http://download.regularlabs.com/?ext=advancedmodulemanager</downloadurl>
+		<downloadurl_pro></downloadurl_pro>
+		<changelog><![CDATA[
+<div style="font-family:monospace;font-size:1.2em;">30-May-2017 : v7.1.4<br /> # [J3.7] Fixes issue with color not being saved when setting in the list<br /><br />02-May-2017 : v7.1.3<br /> # [PRO] Fixes issue with VirtueMart Product assignments throwing an error<br /><br />26-Apr-2017 : v7.1.2<br /> # Fixes issue with disabled/blocked users not appearing in user assignment field<br /> # Fixes issue with potential undefined offset error<br /> # Fixes issue with the site/administrator switch option not showing in the list view<br /> # [PRO] Fixes issue with K2 tag page type assignment not working<br /><br />18-Feb-2017 : v7.1.1<br /> # Fixes issue with match all usergroups setting not working<br /> # Fixes issue with php notice about an undefined property $advancedparams on some setups</div><div style="text-align: right;"><i>...click for more...</i></div>
+		]]></changelog>
+	</extension>
+</extensions>";
